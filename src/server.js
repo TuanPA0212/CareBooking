45d3d90a1af0bd1a564f52dc78392765b077630d
TuanPA <phananhtuan439@gmail.com>
2024-04-03 14:12:27 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+
+require("dotenv").config(); //giúp chạy process.env
+
+let app = express();
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB();
+
+let port = process.env.PORT || 3000;
+// port === undefined => port = 3000
+
+app.listen(port, () => {
+  console.log("Nodejs is running on port", port);
+});
